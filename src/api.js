f7f99bb965613d7509e902f7e04a0e7ba5243ee3
Sergey Maslov <sergey.s.maslov@gmail.com>
2022-07-17 23:42:47 +0300

    initial commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+  headers: {
+    "X-RapidAPI-Key": "e7999340e3mshf8edcb97a53f58dp1f9674jsna54a442abf61",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
