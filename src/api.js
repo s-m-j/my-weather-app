de4042646ecaec41e25b7b5ee30afac7a92b8a79
Sergey Maslov <sergey.s.maslov@gmail.com>
2022-07-18 13:56:49 +0300

    prod

--- src/api.js
+++ src/api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ea7d3ca7a72273a9eeaf149f1a0bb785";
